675e857d25f58e4edcb2f4d4a01b9e7da3d6af00
Rojena <rojenamohamaden@gmail.com>
2024-07-20 10:12:17 +0300

    App initial

---
+++ lib/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+//import 'package:motion_tab_bar/MotionBadgeWidget.dart';
+//import 'package:carousel_slider/carousel_slider.dart';
+//import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+//import 'package:badges/badges.dart' as badges;
+//import 'package:chips_choice/chips_choice.dart';
+//import 'package:buttons_tabbar/buttons_tabbar.dart';
+//import 'package:contained_tab_bar_view/contained_tab_bar_view.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  static const String routeName = "HomeScreen";
+
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ,
+    );
+  }
+}
